d5d5dacc98f951d21eebaa8804c4727df94b0106
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-11-19 21:03:44 +0700

    vercel

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import express from "express";
 import routers from "./routers/index.js"; 
 
 const app = express();
-app.use("/", (req, res) => {
-    res.json({ message: "hello" });
-});
 
 app.use("/api/v1", routers);
 
